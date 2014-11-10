94daeeb814cdc1a70297c8c64673a1630d32b75a
gwyuan <henryontheway@gmail.com>
2014-11-09 21:48:06 -0600

    Create parseRadarData.h

---
+++ parseRadarData.h
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Data{
+  public:
+        string date, time, filename;
+        char plot, side, cycle;
+        float hh, hv, vv, vh, phi1, phi2;
+        int samples;
+        
+};
